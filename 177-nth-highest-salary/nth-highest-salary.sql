0861a503e74c608cf26cc57de69b8f30d6e942a8
SUJITH KULAL <147096935+Sujith-Kulal@users.noreply.github.com>
2025-10-29 22:54:24 +0530

    Time: 511 ms (16.59%) | Memory: 0B (100.00%) - LeetSync

---
+++ 177-nth-highest-salary/nth-highest-salary.sql
@@ -0,0 +1,16 @@
+CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
+BEGIN
+  DECLARE res INT;
+
+  SELECT DISTINCT e1.salary
+  INTO res
+  FROM Employee e1
+  WHERE (
+    SELECT COUNT(DISTINCT e2.salary)
+    FROM Employee e2
+    WHERE e2.salary > e1.salary
+  ) = N - 1
+  LIMIT 1;
+
+  RETURN res;
+END;
